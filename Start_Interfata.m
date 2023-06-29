e5903fd941992d7b5ca949158db9a37c8aba41f7
Narcis-Mihail <82895277+Narcis-Mihail@users.noreply.github.com>
2023-06-30 00:39:24 +0300

    Add files via upload

---
+++ Start_Interfata.m
@@ -0,0 +1,32 @@
+clear all;
+close all;
+clc;
+
+k=1;
+A2=0;
+A3=0;
+Ph2=0;
+Ph3=0;
+n=25;
+N=4;
+Va1=20;
+Va2=-20;
+V1=3;
+Ampl2=3;
+f2=1;
+Tipf2=2;
+Ampl3=2;
+f3=1;
+Tipf3=2;
+TipSemnal=2;
+R1=2;
+TipR1=2;
+R2=1;
+TipR2=2;
+R3=1;
+TipR3=2;
+Rn=3;
+TipRn=2;
+
+
+Interfata(V1,Ampl2,Ampl3,f2,f3,Tipf2,Tipf3,R1,R2,R3,Rn,TipR1,TipR2,TipR3,TipRn,TipSemnal,Va1,Va2,N,n,Ph2,Ph3,A2,A3,k);
